c748241a9df2e3829cee7334f22d7b86ff60e3ff
zackees <z@zackees.com>
2022-07-29 14:45:47 -0700

    update

--- service.py
+++ service.py
@@ -2,8 +2,22 @@
 
 import time
 import os
+import subprocess
 
 DATA_DIR = "/var/data"
+
+def create_webtorrent_files(file: str) -> None:
+  # Check if the magnet file exists
+  magnet_path = os.path.join(DATA_DIR, file + ".magnet")
+  if os.path.exists(magnet_path):
+    return
+  # Create a magnet file
+  #subprocess.run(["mktorrent", "-a", "http://localhost:8080", "-o", magnet_path, os.path.join(DATA_DIR, file)])
+  #return
+  #pass
+  cmd = f'webtorrent-hybrid create -o {file}.torrent'
+  os.system(cmd)
+
 
 def make_index_html() -> None:
   # Scan DATA_DIR for movie files
@@ -11,11 +25,12 @@ def make_index_html() -> None:
   files = os.listdir(DATA_DIR)
   for f in files:
     if f.lower().endswith(".mp4") or f.lower().endswith(".webm"):
+      create_webtorrent_files(f)
       # Make a link to the movie
       html_str += f'<li><a href="{f}">{f}</a></li>'
   html_str += "</ul></body></html>"
   # Write the HTML file
-  with open(os.path.join(DATA_DIR, "index.html"), "w") as f:
+  with open(os.path.join(DATA_DIR, "_index.html"), "w") as f:
     f.write(html_str)
   
 
