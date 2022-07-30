46753b0a68aec64a44102c0f06fde219e9c4fb0a
zackees <z@zackees.com>
2022-07-29 19:05:01 -0700

    remove aggressive caching

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@ set -e
 cp /app/README.md /var/data/README.md
 # Create services and start running them.
 pm2 start "python /app/generate_files.py" --watch /var/data/content --restart-delay=10000 --no-autorestart
-pm2 start "http-server /var/data -p 80 --cors='*'"
+pm2 start "http-server /var/data -p 80 --cors='*' -c-1"
 pm2 save
 # The final command is to keep the container running by streaming
 # the logs.
