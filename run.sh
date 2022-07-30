970102948400b95bcac3b79ffe7c3f2de6abc7f8
zackees <z@zackees.com>
2022-07-29 18:52:23 -0700

    adds run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 set -e
 cp /app/README.md /var/data/README.md
 # Create services and start running them.
-pm2 start "python /app/generate_files.py" --watch /var/data/content --restart-delay=10000
+pm2 start "python /app/generate_files.py" --watch /var/data/content --restart-delay=10000 --no-autorestart
 pm2 start "http-server /var/data -p 80 --cors='*'"
 pm2 save
 # The final command is to keep the container running by streaming
