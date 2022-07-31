369840d1c6dfc4ccbbf85260bf82734d834fd680
zackees <z@zackees.com>
2022-07-31 16:54:15 -0700

    fix

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 set -e
-cd $DATA_DIR && rm *.torrent* && rm *.md5
 cp ./README.md $DATA_DIR/README.md
+cd $DATA_DIR && rm *.torrent* && rm *.md5
 # Generate files will be called whenever the content folder sees changes.
 pm2 start "python ./generate_files.py" --watch $DATA_DIR/content --no-autorestart
 # --cors is required for webseed to work. Allow * and range requests
