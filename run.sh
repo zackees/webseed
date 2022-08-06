614b82917ea7d1d8e28565d975e03a266a9d3eae
zackees <z@zackees.com>
2022-08-06 10:22:04 -0700

    update local dev testing

--- run.sh
+++ run.sh
@@ -3,9 +3,9 @@ rm $DATA_DIR/*.torrent* && rm  $DATA_DIR/*.md5
 set -e
 # --cors is required for webseed to work. Allow * and range requests
 # are explicitly set.
-pm2 start "http-server $DATA_DIR -p 80 --cors='*' -c-1"
+pm2 start http-server -- $DATA_DIR -p 80 --cors='*' -c-1
 # Generate files will be called whenever the content folder sees changes.
-pm2 start "python ./generate_files.py" --watch $DATA_DIR/content --no-autorestart
+pm2 start python -- ./generate_files.py --watch $DATA_DIR/content --no-autorestart
 pm2 save
 # The final command is to keep the container running by streaming
 # the logs.
--- run_dev.sh
+++ run_dev.sh
@@ -1,4 +1,6 @@
 set -e
 mkdir -p .temp
+export DOMAIN_NAME=localhost
 export DATA_DIR=.temp
+export STUN_SERVERS='"stun:relay.socket.dev:443", "stun:global.stun.twilio.com:3478"'
 ./run.sh
